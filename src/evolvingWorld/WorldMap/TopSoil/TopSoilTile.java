936a762b927e446dede2955e274982dfa58d61cf
Dynisious <daniel@bechaz.com>
2015-09-10 22:12:41 +1000

    Atmosphere Tiles added
    I've added in Atmosphere tiles and cleaned up the other tile classes.

--- src/evolvingWorld/WorldMap/TopSoil/TopSoilTile.java
+++ src/evolvingWorld/WorldMap/TopSoil/TopSoilTile.java
@@ -1,6 +1,7 @@
 package EvolvingWorld.WorldMap.TopSoil;
 
 import EvolvingWorld.Events.Updateable;
+import EvolvingWorld.WorldMap.Tile;
 /**
  * <p>
  * Holds all the values associated with the topsoil on a tile of the game
@@ -9,12 +10,12 @@ import EvolvingWorld.Events.Updateable;
  * @author Dynisious 08/09/2015
  * @versions 0.1.1
  */
-public class TopSoilTile extends Updateable {
+public class TopSoilTile extends Updateable implements Tile {
     private double fertility; //The fertility of the soil in this tile.
-    public double getFertility() {
+    public final double getFertility() {
         return fertility;
     }
-    public void setFertitlity(final double val) throws ArithmeticException {
+    public final void setFertitlity(final double val) throws ArithmeticException {
         if (val < 0 || val > 1) {
             throw new ArithmeticException(
                     "ERROR : The parameter val has limits 0 <= val <= 1, val=" + val);
@@ -22,10 +23,10 @@ public class TopSoilTile extends Updateable {
         fertility = val;
     }
     private double waterCont; //The water content of the soil in this tile.
-    public double getWaterCont() {
+    public final double getWaterCont() {
         return waterCont;
     }
-    public void setWaterCont(final double val) throws ArithmeticException {
+    public final void setWaterCont(final double val) throws ArithmeticException {
         if (val < 0 || val > 1) {
             throw new ArithmeticException(
                     "ERROR : The parameter val has limits 0 <= val <= 1, val=" + val);
@@ -33,10 +34,10 @@ public class TopSoilTile extends Updateable {
         waterCont = val;
     }
     private double pollution; //How polluted the soils are in this tile.
-    public double getPollution() {
+    public final double getPollution() {
         return pollution;
     }
-    public void setPollution(final double val) throws ArithmeticException {
+    public final void setPollution(final double val) throws ArithmeticException {
         if (val < 0 || val > 1) {
             throw new ArithmeticException(
                     "ERROR : The parameter val has limits 0 <= val <= 1, val=" + val);
@@ -44,6 +45,9 @@ public class TopSoilTile extends Updateable {
         pollution = val;
     }
     private int soilType; //The type of TopSoil contained in this tile.
+    public final int getSoilType() {
+        return soilType;
+    }
     /**
      * <p>
      * Sets the type of soil in the TopSoilTile and changes it's values to match
@@ -51,12 +55,35 @@ public class TopSoilTile extends Updateable {
      *
      * @param soilType The type of soil to be kept in this TopSoilTile.
      */
-    public void setSoilType(final int soilType) {
+    public final void setSoilType(final int soilType) {
         this.soilType = soilType;
     }
 
+    /**
+     * <p>
+     * Creates and returns a new TopSoilTile of the passed type.</p>
+     *
+     * @param soilType The type of soil in this TopSoilTile.
+     */
     public TopSoilTile(final int soilType) {
         setSoilType(soilType);
+    }
+
+    /**
+     * <p>
+     * Creates and returns a new TopSoilTile with the passed values.</p>
+     *
+     * @param fertility The fertility of the soil in this TopSoilTile.
+     * @param soilType  The type of soil in this TopSoilTile.
+     * @param waterCont The water content of the soil in this TopSoilTile.
+     * @param pollution The pollution of the soil in this TopSoilTile.
+     */
+    public TopSoilTile(final double fertility, final double waterCont,
+                       final double pollution, final int soilType) {
+        setSoilType(soilType);
+        setFertitlity(fertility);
+        setWaterCont(waterCont);
+        setPollution(pollution);
     }
 
 }
